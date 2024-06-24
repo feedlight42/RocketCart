433d33c8fa6495c827bcb70cc50c11e66fc66b3a
Eraiyanbu <eraiyanbupandian@gmail.com>
2024-06-24 18:39:56 +0530

    initialthree

---
+++ src/main/java/com/example/RocketCart/controller/AuthController.java
@@ -0,0 +1,62 @@
+package com.example.RocketCart.controller;
+
+import com.example.RocketCart.model.Customer;
+import com.example.RocketCart.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+//    @PostMapping("/signup")
+//    public ResponseEntity<?> signUp(@RequestBody User user) {
+//        if (userRepository.existsByUsername(user.getUsername())) {
+//            return ResponseEntity.badRequest().body("Username is already taken!");
+//        }
+//
+//        // Encrypt the password before storing in the database
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        userRepository.save(user);
+//
+//        return ResponseEntity.ok("User registered successfully");
+//    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> signUpRequest) {
+
+        String username = signUpRequest.get("username");
+        String password = signUpRequest.get("password");
+
+        if (username == null || password == null) {
+            return ResponseEntity.badRequest().body("Username and password are required");
+        }
+
+        Customer user;
+        user = customerRepository.findByUsername(username);
+
+        if (user == null || !user.getPassword().equals(password)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+
+        return ResponseEntity.ok("Login successful");
+
+    }
+
+
+}
--- src/main/java/com/example/RocketCart/repository/CustomerRepository.java
+++ src/main/java/com/example/RocketCart/repository/CustomerRepository.java
@@ -1,8 +1,15 @@
 package com.example.RocketCart.repository;
 import com.example.RocketCart.model.Customer;
+import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
-
+    Customer findByUsername(String username);
 }
